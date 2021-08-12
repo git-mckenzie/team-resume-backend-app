12930233d993d32f08d08379fefacda261733460
Mabelys Arias <MabelysA@gmail.com>
2021-08-12 15:27:15 -0400

    modifies education and skills controllers

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -22,7 +22,7 @@ class EducationController < ApplicationController
 
   def show
     education_id = params[:id]
-    parks = Education.find(education_id)
+    education = Education.find(education_id)
     render json: education
   end
 
@@ -32,19 +32,20 @@ class EducationController < ApplicationController
     education.start_date = params["start_date"] || education.start_date
     education.end_date = params["end_date"] || education.end_date
     education.degree = params["degree"] || education.degree
-    education.size = params["size"] || education.size
-    parks.image_url = params["image_url"] || parks.image_url
-    if parks.save
-      render json: parks
+    education.university_name = params["university_name"] || education.university_name
+    education.details = params["details"] || education.details
+    education.student_id = params["student_id"] || education.student_id
+    if education.save
+      render json: education
     else
-      render json: { errors: parks.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: education.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    parks_id = params[:id]
-    parks = Park.find_by(id: parks_id)
-    parks.destroy
-    render json: { message: "You have deleted the park!" }
+    education_id = params[:id]
+    education = Education.find_by(id: education_id)
+    education.destroy
+    render json: { message: "Confirmation: Deleted!" }
   end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,41 @@
 class SkillsController < ApplicationController
+  def index
+    skills = Skill.all
+    render json: skills
+  end
+
+  def create
+    skills = Skill.new(
+      name: params["name"],
+    )
+    if skills.save
+      render json: skills
+    else
+      render json: { errors: skills.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    skills_id = params[:id]
+    skills = Skill.find(skills_id)
+    render json: skills
+  end
+
+  def update
+    skills_id = params[:id]
+    skills = Skill.find_by(id: skills_id)
+    skills.name = params["name"] || skills.name
+    if skills.save
+      render json: skills
+    else
+      render json: { errors: skills.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    skills_id = params[:id]
+    skills = Skill.find_by(id: skills_id)
+    skills.destroy
+    render json: { message: "Confirmation: Deleted!" }
+  end
 end
