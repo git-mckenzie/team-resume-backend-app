29d3d31ee6d4dc0db6ba457f17fcb8caf92c0815
Pinny Geliebter <pgibralter@gmail.com>
2021-08-12 16:53:57 -0400

    adds resume controller index and show routes

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,12 @@
 class ResumesController < ApplicationController
+  def index
+    student = Student.all
+    render json: student
+  end
+
+  def show
+    student_id = params["id"]
+    student = Student.find(student_id)
+    render json: student
+  end
 end
