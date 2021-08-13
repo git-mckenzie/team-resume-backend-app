c5dbfc52fd8b058a64aff205745e033cb6ad8178
Jordan <jcharrison12@gmail.com>
2021-08-13 14:23:22 -0500

    Adds frontend to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins "localhost:8080", "https://online-resumes.netlify.app/", "https://clever-goldberg-6e1c93.netlify.app/"
 
     resource "*",
       headers: :any,
